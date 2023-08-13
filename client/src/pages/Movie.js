b6387dc1e5509d7c9ca4d2dfd5464abc78417982
isacpxc <isacpxc@gmail.com>
2023-08-13 15:49:04 -0300

    pages

---
+++ client/src/pages/Movie.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Movie extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Movie</h1>
+      </div>
+    );
+  }
+}
+
+export default Movie;
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SignUp extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>SignUp</h1>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
